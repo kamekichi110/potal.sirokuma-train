609bd808eb26a756fff3610a1d915e3aea55ce0e
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-03-04 13:52:56 +0900

    e

--- ja-JP/js/push-log.js
+++ ja-JP/js/push-log.js
@@ -1,10 +1,10 @@
 Push.create('しろくま鉄道ポータル(system)', {
-    body: 'ポータルバージョン(v1.2)',
+    body: 'お知らせはありませ',
     icon: 'icon.png',
-    timeout: 8000, // 通知が消えるタイミング
+    timeout: 15000, // 通知が消えるタイミング
     vibrate: [100, 100, 100], // モバイル端末でのバイブレーション秒数
     onClick: function() {
     // 通知がクリックされた場合の設定
-    window.location.href = "https://kamekichi110.github.io/potal.sirokuma-train/ja-JP/";
+    window.location.href = "https://kamekichi110.github.io/potal.sirokuma-train/ja-JP/notice/";
     }
     });
