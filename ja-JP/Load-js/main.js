f819444f77faed3d044624ea85e2f31632c3d52f
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-03-08 21:35:56 +0900

    e

---
+++ ja-JP/Load-js/main.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    setTimeout(() => {
+        const loader = document.querySelector(".loader")
+        loader.classList.add("loaded")
+        const content = document.querySelector("body")
+        body.style.visibility = "visible"
+    }, 1300);
+};
