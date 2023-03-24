a3d3554451365fc73bb41e7f47cc105e171d55f0
kamekichi110 <123218419+kamekichi110@users.noreply.github.com>
2023-03-24 16:04:24 +0900

    e

--- ja-JP/serviceWorker.js
+++ ja-JP/serviceWorker.js
@@ -73,3 +73,19 @@ self.onnotificationclick = function (event) {
 
   runFunctionString(event.notification.data.onClick);
 }
+
+// インストール
+self.addEventListener('install', function (e) {
+    console.info('install', e);
+    });
+
+    // アクティベート
+    self.addEventListener('activate', function (e) {
+        console.info('activate', e);
+        });
+
+        // フェッチ
+        self.addEventListener('fetch', function (e) {
+            console.info('fetch', e);
+            });
+            
